27068c63ec8aa12bdeae4c9de3418a28170fc3dd
travis-tyler <travisatyler@gmail.com>
2021-01-22 20:12:49 -0600

    added function to generate support/resistance lines

---
+++ funcs.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import ui
+
+def is_support(df, i):
+    '''Determines if support line'''
+    support = df['low'][i] < df['low'][i-1]\
+        and df['low'][i] < df['low'][i+1]\
+        and df['low'][i+1] < df['low'][i+2]\
+        and df['low'][i-1] < df['low'][i-2]
+    return support
+
+def is_resistance(df, i):
+    '''Determines if resistance line'''
+    resistance = df['high'][i] > df['high'][i-1]\
+        and df['high'][i] > df['high'][i+1]\
+        and df['high'][i+1] > df['high'][i+2]\
+        and df['high'][i-1] > df['high'][i-2]
+    return resistance
+
+def make_df(result):
+    '''Creates and returns dataframe using pandas'''
+    hist = {'date':[], 
+        'low':[], 
+        'high':[], 
+        'open':[], 
+        'close':[], 
+        'volume':[]}
+
+    for i in result:
+        hist['date'].append(i['begins_at'][:10])
+        hist['low'].append(i['low_price'])
+        hist['high'].append(i['high_price'])
+        hist['open'].append(i['open_price'])
+        hist['close'].append(i['close_price'])
+        hist['volume'].append(i['volume'])
+
+    df = pd.DataFrame(hist)
+    df['date'] = pd.to_datetime(df['date'])
+    cols = df.columns.drop('date')
+    df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
+
+    return df
+
+def get_s_r(df):
+    '''Loops through DF to determine and return clear support and resistance lines'''
+    s = np.mean(df['high'] - df['low'])
+
+    levels = [] 
+
+    for i in range(2, df.shape[0]-2):
+        if is_support(df, i):
+            l = df['low'][i]
+
+            if np.sum([abs(l-x) < s for x in levels]) == 0:
+                levels.append((i, l))
+                ui.success(f'{l} | SUPPORT')
+
+        elif is_resistance(df, i):
+            l = df['high'][i]
+
+            if np.sum([abs(l-x) < s for x in levels]) == 0:
+                levels.append((i, l))
+
+                ui.error(f'{l} | RESISTANCE')
+
+    # return levels
--- robin.py
+++ robin.py
@@ -1,16 +1,14 @@
-import click
-import robin_stocks as rh
-import ui
-from config import user, pw
+import click, json, robin_stocks as rh
+import ui, funcs, config
 
 @click.group()
 def main():
-    rh.login(user, pw)
+    rh.login(config.user, config.pw)
 
 @main.command(help='Gets the information associated with the accounts profile being held by Robinhood.')
 @click.option('--fetch', type=click.STRING)
 def info(fetch):
-    print(f'Fetching {fetch}....')
+    print(f'\nFetching {fetch}....\n')
     result = rh.load_account_profile()
 
     if fetch is not None:
@@ -24,12 +22,11 @@ def quote(symbols):
     quotes = rh.get_quotes(symbols)
 
     for quote in quotes:
-        ui.success(f"{quote['symbol']} | {quote['ask_price']}")
-
+        ui.success(f"\n{quote['symbol']} | {quote['last_extended_hours_trade_price']}\n")
 
 @main.command(help='Gets help for all stocks in your watchlist')
 def watchlist():
-    print('Getting quotes for watchlist....')
+    print('\nGetting quotes for watchlist....\n')
     
     with open('watchlist') as f:
         symbols = f.read().splitlines()
@@ -49,7 +46,7 @@ def buy(quantity, symbol, limit):
         ui.success(f"Bying {quantity} of {symbol} at ${limit}")
         result = rh.order_buy_limit(symbol, quantity, limit)
     else:
-        ui.success(f'Bying {quantity} of {symbol} at market price....')
+        ui.success(f'Bying {quantity} of {symbol} at market price....\n')
         result = rh.order_buy_market(symbol, quantity)
 
     ui.chec_ref(result)
@@ -60,10 +57,10 @@ def buy(quantity, symbol, limit):
 @click.option('--limit', type=click.FLOAT)
 def sell(quantity, symbol, limit):
     if limit is not None:
-        ui.success(f"Selling {quantity} of {symbol} at ${limit}")
+        ui.success(f"\nSelling {quantity} of {symbol} at ${limit}\n")
         result = rh.order_sell_limit(symbol, quantity, limit)
     else:
-        ui.success(f'Selling {quantity} of {symbol} at market price....')
+        ui.success(f'\nSelling {quantity} of {symbol} at market price....\n')
         result = rh.order_sell_market(symbol, quantity)
 
     ui.chec_ref(result)
@@ -72,7 +69,7 @@ def sell(quantity, symbol, limit):
 @click.argument('amount', type=click.FLOAT)
 @click.argument('symbol', type=click.STRING)
 def buy_frac(amount, symbol):
-    ui.success(f'Buying ${amount} of {symbol}....')
+    ui.success(f'\nBuying ${amount} of {symbol}....\n')
     result = rh.order_buy_fractional_by_price(symbol, amount)
 
     ui.chec_ref(result)
@@ -82,7 +79,7 @@ def buy_frac(amount, symbol):
 @click.option('--interval', type=click.STRING)
 @click.option('--span', type=click.STRING)
 def history(symbol, interval, span):
-    ui.success(f'Get {interval} stock historicals for {symbol} for the past {span}....')
+    ui.success(f'\nGet {interval} stock historicals for {symbol} for the past {span}....\n')
     result = rh.get_stock_historicals(symbol, interval, span)
 
     for i in result:
@@ -90,61 +87,44 @@ def history(symbol, interval, span):
             ui.success(f'{x}: {i[x]}')
         ui.bar()
 
-    # ui.success(result)
-
 @main.command(help='Generates support and resistance data from stock history by symbol.')
 @click.argument('symbol', type=click.STRING)
 @click.option('--interval', type=click.STRING)
 @click.option('--span', type=click.STRING)
 def get_lines(symbol, interval, span):
-    ui.success(f'Get {interval} stock historicals for {symbol} for the past {span}....')
+    ui.success(f'\nGetting {interval} stock historicals for {symbol} for the past {span}....\n')
     result = rh.get_stock_historicals(symbol, interval, span)
 
-    hist = {'dates':[], 
-            'lows':[], 
-            'highs':[], 
-            'opens':[], 
-            'closes':[], 
-            'volumes':[]}
+    price = rh.get_quotes(symbol)[0]['last_extended_hours_trade_price']
 
-    for i in result:
-        hist['dates'].append(i['begins_at'][:10])
-        hist['lows'].append(i['low_price'])
-        hist['highs'].append(i['high_price'])
-        hist['opens'].append(i['open_price'])
-        hist['closes'].append(i['close_price'])
-        hist['volumes'].append(i['volume'])
+    ui.bar()
+    ui.success(f"Value: {price}")
+    ui.bar()
+    df = funcs.make_df(result)
+    funcs.get_s_r(df)
+    ui.bar()
 
-    ui.success(hist)
-
-
-
+    # with open('data.txt', 'w') as outfile:
+    #     json.dump(result, outfile)
 
 @main.command(help='Gets the information associated with the portfolios profile.')
 def portfolio():
     print('Loading portfolio profile....')
     result = rh.load_portfolio_profile()
-
     ui.success(result)
 
 @main.command(help='Gets the information associated with the portfolios profile.')
 def investments():
-    print('Loading investment profile....')
+    print('\nLoading investment profile....\n')
     result = rh.load_investment_profile()
-
     ui.success(result)
     
-@main.command(help='Represents the historicl data for a stock.')
+@main.command(help='Represents the historical data for a stock.')
 @click.argument('symbol', type=click.STRING)
 def ratings(symbol):
-    ui.success(f'Get stock ratings for {symbol}....')
+    ui.success(f'\nGet stock ratings for {symbol}....\n')
     result = rh.get_ratings(symbol)
-
     ui.success(result)
-
-
-
 
 if __name__ == '__main__':
     main()
-
