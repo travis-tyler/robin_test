82f7f7602709947a61c29472c6a35bd68e51d8c4
travis-tyler <travisatyler@gmail.com>
2021-01-22 20:12:49 -0600

    added function to generate support/resistance lines

--- ui.py
+++ ui.py
@@ -7,7 +7,7 @@ def error(message):
     click.echo(click.style(str(message), fg='red', bold=True))
 
 def bar():
-    click.echo(click.style('=============', fg='red', bold=True))
+    click.echo(click.style('=============', fg='green', bold=True))
 
 def chec_ref(result):
     if 'ref_id' in result:
